f31619e70466ce1eaf5fb5afa31dda859713b194
hoanvuonq <hoanvuonq2002@gmail.com>
2024-11-01 13:47:05 +0700

    Update 5s

--- src/pages/evaluate/fomEvaluate.tsx
+++ src/pages/evaluate/fomEvaluate.tsx
@@ -48,7 +48,7 @@ const FomEvaluate: React.FC<IPopupProps> = ({ onClose, previewData }) => {
         setLoading(true);
 
         try {
-            await new Promise((resolve) => setTimeout(resolve, 7000));
+            await new Promise((resolve) => setTimeout(resolve, 5000));
             const backgroundImageKey = Object.keys(images).find((key) => images[key] === randomTrip.backgroundImage);
 
             if (!backgroundImageKey) {
